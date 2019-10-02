79d6fe139aa9a166741e2b6282a0fd008d6667d8
Surendarmurugesan <53648189+Surendarmurugesan@users.noreply.github.com>
2019-10-02 10:06:01 +0530

    Create cit.groovy

---
+++ cit.groovy
@@ -0,0 +1,30 @@
+node("master") {
+    wrks = env.WORKSPACE
+    stage("Prepare"){
+        println("Preparing...")
+        git(
+                url: "git@github.com:srikrishnaprakash/pls.git",
+                branch: "master"
+	    )
+        dir('config') {
+          git(
+                url: "git@github.com:srikrishnaprakash/conf.git",
+                branch: "master"
+	        )  
+        }
+        
+        
+    }
+    stage("Clone Application"){
+        load 'app/clne.groovy'
+    }
+    stage("Build"){
+        load 'app/bld.groovy'
+    }
+    stage("Test"){
+        println("Testing...")
+    }
+    stage("Deploy"){
+        println("Deploying...")
+    }
+}
