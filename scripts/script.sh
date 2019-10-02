8fddff2be6395c17b8f6a0379321d838187b790c
Surendarmurugesan <53648189+Surendarmurugesan@users.noreply.github.com>
2019-10-02 12:19:54 +0530

    Update script.sh

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,5 +1,4 @@
-catalina stop
 rm -rf /opt/servers/tomcat8/webapps/arapp.war
 rm -rf /opt/servers/tomcat8/webapps/arapp
 cp ~/.jenkins/workspace/Ziffity/Projects/CICD_Zyo/appl/target/arapp.war /opt/servers/tomcat8/webapps
-catalina start
+
