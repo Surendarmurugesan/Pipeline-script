080b7f7ef084a7907952354d1a3abf1acb475f05
surendar <surendarmurugesan96@gmail.com>
2019-10-02 12:12:41 +0530

    sdc

--- script.sh
+++
@@ -1,4 +0,0 @@
-catalina stop
-rm -rf /opt/servers/tomcat8/webapps/arapp
-cp -f /opt/servers/tomcat8/webapps $works/appl/target/arapp.war
-catalina start
---
+++ scripts/script.sh
@@ -0,0 +1,5 @@
+catalina stop
+rm -rf /opt/servers/tomcat8/webapps/arapp.war
+rm -rf /opt/servers/tomcat8/webapps/arapp
+cp ~/.jenkins/workspace/Ziffity/Projects/CICD_Zyo/appl/target/arapp.war /opt/servers/tomcat8/webapps
+catalina start
